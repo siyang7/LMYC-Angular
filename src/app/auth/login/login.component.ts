5c4c42715115ef21a0ff35531a752303c3e9fced
Bryan Brotonel <mrbryanbrotonel@gmail.com>
2018-03-31 15:46:48 -0700

    Added login page validation

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,26 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from "@angular/forms";
+
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+	selector: 'app-login',
+	templateUrl: './login.component.html',
+	styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
-  loginForm: FormGroup;
 
-  constructor() {
-    console.log(this.loginForm);
-    //this.loginForm.reset();
-   }
+export class LoginComponent implements onInit {
 
-  ngOnInit() {
-    this.loginForm = new FormGroup({
-      email: new FormControl(null, [
-        Validators.required,
-        Validators.pattern("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")
-      ]),
-      password: new FormControl(null, Validators.required)
-    });
-  }
+	user = { email: '', password: '' };
+
+	loginForm: FormGroup;
+
+	ngOnInit(): void {
+		this.loginForm = new FormGroup({
+			'email': new FormControl(this.user.email, [
+				Validators.required,
+				Validators.pattern("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")
+			]),
+			'password': new FormControl(this.user.password, Validators.required)
+		});
+	}
+
+	get email() { return this.loginForm.get('email'); }
+
+	get password() { return this.loginForm.get('password'); }
 
 }
